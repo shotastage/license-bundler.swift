a028eeb53b49daf79741ea15f9ce39dc0c43a1e6
Shota Shimazu <hornet.live.mf@gmail.com>
2022-04-20 01:29:51 +0900

    revise

--- Sources/LicenseBundleMaker/Reader.swift
+++ Sources/LicenseBundleMaker/Reader.swift
@@ -1,6 +1,6 @@
 //
 //  Reader.swift
-//  
+//
 //
 //  Created by Shota Shimazu on 2022/04/09.
 //
@@ -8,10 +8,21 @@
 import Foundation
 
 class Reader {
-    
-    let libPath: String = ""
-    
+
+    let libPath: String
+
+    let fileIdentifier: String
+
+    init(path: String, identifier: String) {
+        self.libPath = path
+        self.fileIdentifier = identifier
+    }
+
     func read() {
-        
+
+    }
+
+    func dirWalk() {
+
     }
 }
