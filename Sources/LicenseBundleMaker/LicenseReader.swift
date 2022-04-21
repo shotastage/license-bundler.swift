21c9e23637d7f277762e978dc94d9445d04d1555
Shota Shimazu <hornet.live.mf@gmail.com>
2022-04-22 00:13:12 +0900

    add identifer

---
+++ Sources/LicenseBundleMaker/LicenseReader.swift
@@ -0,0 +1,15 @@
+//
+//  LicenseReader.swift
+//  
+//
+//  Created by Shota Shimazu on 2022/04/22.
+//
+
+import Foundation
+
+let fileIdentifier = [
+    "LICENSE",
+    "LICENSE.md",
+    "License.md",
+    "LICENSE.txt"
+]
